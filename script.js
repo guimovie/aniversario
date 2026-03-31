fb5aa3b326f7ab4a5c6600fd0d8440989178c1e6
guimovie <gduartedasilva3@gmail.com>
2026-03-31 02:48:53 -0300

    .

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 function clicar() {
-    window.alert('teste')
     var main = window.document.getElementById('main')
     var img = document.createElement('img')
     img.setAttribute('id', 'foto')
     img.setAttribute('src', 'ozzy.png')
-    main.innerHTML = "<div id='txt'><p>Oi, eu sou o osvaldinho e vim desejar um feliz aniversário para a pessoa mais incrivel e especial do mundo, que graças a ela, eu tenho um lar e muito amor, te amo muitão mamãe, e o papai também te ama muitão.</p></div>"
+    main.innerHTML = "<div id='txt'><p>Oi, eu sou o osvaldinho e vim desejar um feliz aniversário para a pessoa mais incrivel e especial do mundo, que graças a ela, eu tenho um lar e muito amor, te amo muitão mamãe, e o papai também te ama muitão. Muitos  lambeijos em  você!</p></div>"
     main.appendChild(img)
-    
-
 }
